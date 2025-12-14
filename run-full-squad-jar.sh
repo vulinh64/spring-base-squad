672f19e2c1758213dc76bc8cab0f4585474a7011
Linh Nguyen <nguyenvulinh64@outlook.com>
2025-12-14 21:45:17 +0700

    Modify scripts

--- run-full-squad-jar.sh
+++ run-full-squad-jar.sh
@@ -4,74 +4,40 @@ set -euo pipefail
 # Check if Docker daemon is running
 docker info >/dev/null 2>&1 || { echo "Error: Docker daemon is not running."; echo "Please start Docker and run this script again."; exit 1; }
 
-# Remove old directories if they exist
-rm -rf spring-base spring-base-event
+# Remove existing build folders to prepare for fresh checkout
+rm -rf ./build/spring-base
+rm -rf ./build/spring-base-event
+rm -rf ./build/spring-base-commons
 
-# Clone repositories
-git clone https://github.com/vulinh64/spring-base.git
-git clone https://github.com/vulinh64/spring-base-event.git
+# Clone repositories with shallow depth to save bandwidth
+git clone --depth 1 https://github.com/vulinh64/spring-base.git ./build/spring-base
+git clone --depth 1 https://github.com/vulinh64/spring-base-event.git ./build/spring-base-event
+git clone --depth 1 https://github.com/vulinh64/spring-base-commons.git ./build/spring-base-commons
 
-chmod +x ./spring-base/mvnw
-chmod +x ./spring-base-event/mvnw
+# Remove .git directories to clean up version control metadata
+rm -rf ./build/spring-base/.git
+rm -rf ./build/spring-base-event/.git
+rm -rf ./build/spring-base-commons/.git
 
-./spring-base/mvnw clean install -DskipTests -f ./spring-base/pom.xml
-./spring-base-event/mvnw clean install -DskipTests -f ./spring-base-event/pom.xml
+# Build the spring-base-commons artifact using Maven
+./build/spring-base-commons/mvnw clean install -f ./build/spring-base-commons/pom.xml
 
-# Stop and remove old containers/images
+# Copy the built JAR file to both project build directories for Docker builds
+mkdir -p ./build/spring-base/build/spring-base-commons/target
+mkdir -p ./build/spring-base-event/build/spring-base-commons/target
+cp ./build/spring-base-commons/target/spring-base-commons-1.0.0.jar ./build/spring-base/build/spring-base-commons/target/spring-base-commons-1.0.0.jar
+cp ./build/spring-base-commons/target/spring-base-commons-1.0.0.jar ./build/spring-base-event/build/spring-base-commons/target/spring-base-commons-1.0.0.jar
+
+# Build both Spring Boot applications using Maven on host OS (skipping tests)
+./build/spring-base/mvnw clean install -DskipTests -f ./build/spring-base/pom.xml
+./build/spring-base-event/mvnw clean install -DskipTests -f ./build/spring-base-event/pom.xml
+
+# Stop existing containers, remove old images, and start fresh containers
 docker compose down || true
 docker rmi --force spring-base:1.0.0 || true
 docker rmi --force spring-base-event:1.0.0 || true
 docker compose -f docker-compose-1.yml up --detach
 
-KEYCLOAK_CONTAINER=keycloak
-KCADM_PATH=/opt/keycloak/bin/kcadm.sh
-KEYCLOAK_REALM=spring-base
-CLIENT_ID=spring-base-client
-KEYCLOAK_OVERLORD=admin
-KEYCLOAK_ADMIN_PASSWORD=123456
-
-# Configure KCADM credentials
-echo "Configuring KCADM credentials..."
-docker exec "$KEYCLOAK_CONTAINER" "$KCADM_PATH" config credentials --server http://localhost:8080 --realm master --user "$KEYCLOAK_OVERLORD" --password "$KEYCLOAK_ADMIN_PASSWORD"
-
-docker exec "$KEYCLOAK_CONTAINER" "$KCADM_PATH" create realms -s realm="$KEYCLOAK_REALM" -s enabled=true
-
-echo "Creating client [$CLIENT_ID]..."
-CLIENT_UUID=$(docker exec "$KEYCLOAK_CONTAINER" "$KCADM_PATH" create clients -r "$KEYCLOAK_REALM" -s clientId="$CLIENT_ID" -s enabled=true -s publicClient=true -s directAccessGrantsEnabled=true -i)
-
-# --- Role and User Setup ---
-ROLE_ADMIN=ADMIN
-ROLE_POWER_USER=POWER_USER
-ROLE_USER=USER
-
-ADMIN_USERNAME=administrator
-POWER_USER_USERNAME=power_user
-USER_USERNAME=user
-
-docker exec "$KEYCLOAK_CONTAINER" "$KCADM_PATH" create clients/$CLIENT_UUID/roles -r "$KEYCLOAK_REALM" -s name="$ROLE_ADMIN"
-docker exec "$KEYCLOAK_CONTAINER" "$KCADM_PATH" create clients/$CLIENT_UUID/roles -r "$KEYCLOAK_REALM" -s name="$ROLE_POWER_USER"
-docker exec "$KEYCLOAK_CONTAINER" "$KCADM_PATH" create clients/$CLIENT_UUID/roles -r "$KEYCLOAK_REALM" -s name="$ROLE_USER"
-
-echo "Creating admin user [$ADMIN_USERNAME]..."
-docker exec "$KEYCLOAK_CONTAINER" "$KCADM_PATH" create users -r "$KEYCLOAK_REALM" -s username="$ADMIN_USERNAME" -s enabled=true -s email=administrator@email.com -s firstName=Administrator -s lastName=User
-docker exec "$KEYCLOAK_CONTAINER" "$KCADM_PATH" set-password -r "$KEYCLOAK_REALM" --username "$ADMIN_USERNAME" --new-password 123456
-
-echo "Assigning client role [$ROLE_ADMIN] to user [$ADMIN_USERNAME] in client [$CLIENT_ID]..."
-docker exec "$KEYCLOAK_CONTAINER" "$KCADM_PATH" add-roles -r "$KEYCLOAK_REALM" --uusername "$ADMIN_USERNAME" --cclientid "$CLIENT_ID" --rolename "$ROLE_ADMIN"
-echo "Created user [$ADMIN_USERNAME] with password 123456 and client role [$ROLE_ADMIN] in client [$CLIENT_ID]"
-
-echo "Creating regular user [$USER_USERNAME]..."
-docker exec "$KEYCLOAK_CONTAINER" "$KCADM_PATH" create users -r "$KEYCLOAK_REALM" -s username="$USER_USERNAME" -s enabled=true -s email=user@email.com -s firstName=Normal -s lastName=User
-docker exec "$KEYCLOAK_CONTAINER" "$KCADM_PATH" set-password -r "$KEYCLOAK_REALM" --username "$USER_USERNAME" --new-password 123456
-
-echo "Assigning client role [$ROLE_USER] to user [$USER_USERNAME] in client [$CLIENT_ID]..."
-docker exec "$KEYCLOAK_CONTAINER" "$KCADM_PATH" add-roles -r "$KEYCLOAK_REALM" --uusername "$USER_USERNAME" --cclientid "$CLIENT_ID" --rolename "$ROLE_USER"
-echo "Created user [$USER_USERNAME] with password 123456 and client role [$ROLE_USER] in client [$CLIENT_ID]"
-
-echo "Creating power user [$POWER_USER_USERNAME]..."
-docker exec "$KEYCLOAK_CONTAINER" "$KCADM_PATH" create users -r "$KEYCLOAK_REALM" -s username="$POWER_USER_USERNAME" -s enabled=true -s email=power_user@email.com -s firstName=Power -s lastName=User
-docker exec "$KEYCLOAK_CONTAINER" "$KCADM_PATH" set-password -r "$KEYCLOAK_REALM" --username "$POWER_USER_USERNAME" --new-password 123456
-
-echo "Assigning client role [$ROLE_POWER_USER] to user [$POWER_USER_USERNAME] in client [$CLIENT_ID]..."
-docker exec "$KEYCLOAK_CONTAINER" "$KCADM_PATH" add-roles -r "$KEYCLOAK_REALM" --uusername "$POWER_USER_USERNAME" --cclientid "$CLIENT_ID" --rolename "$ROLE_POWER_USER"
-echo "Created user [$POWER_USER_USERNAME] with password 123456 and client role [$ROLE_POWER_USER] in client [$CLIENT_ID]"
+# Initialize Keycloak data (realm, client, roles, and users)
+chmod +x ./build/spring-base/create-keycloak-data.sh
+./build/spring-base/create-keycloak-data.sh
